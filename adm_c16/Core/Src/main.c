7bb3d0698bf484e67fcce3fb24d709445a816731
vbelaunde <VictorBelaunde@NTK0743.local>
2022-12-09 10:55:39 -0300

    Ejercicio 6 en C

--- adm_c16/Core/Src/main.c
+++ adm_c16/Core/Src/main.c
@@ -188,7 +188,14 @@ void filtroVentana10(uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitu
  */
 void pack32to16 (int32_t * vectorIn, int16_t *vectorOut, uint32_t longitud)
 {
+	{
+		uint32_t i = 0;
 
+		for(i = 0; i < longitud; i++)
+		{
+			*(vectorOut+i) = (int16_t)((*(vectorIn+i))>>16);
+		}
+	}
 }
 
 /*
