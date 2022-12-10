9443aae7cc565ba75460b12268c80edb336b4771
vbelaunde <VictorBelaunde@NTK0743.local>
2022-12-10 08:54:07 -0300

    Ejercicio 8 en asembler

--- adm_c16/Core/Src/main.c
+++ adm_c16/Core/Src/main.c
@@ -79,8 +79,11 @@ void asm_productoEscalar16(uint16_t * vectorIn, uint16_t * vectorOut, uint32_t l
 //Funcion asembler Eje 4
 void asm_productoEscalar12 (uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitud, uint16_t escalar);
 
-
+//Funcion asembler Eje 7
 int32_t asm_max (int32_t * vectorIn, uint32_t longitud);
+
+//Funcion asembler 8
+void asm_downsampleM (int32_t * vectorIn, int32_t * vectorOut, uint32_t longitud, uint32_t N);
 
 /* USER CODE END PFP */
 
